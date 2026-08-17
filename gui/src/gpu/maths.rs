6a8fffe5ac5e1f1805e7497696c78e68b63a13a3
Léonard <leonard.panichi@gmail.com>
2026-08-17 19:35:18 +0200

    remove dead code for mercator, add test on latlon mappings

--- gui/src/gpu/maths.rs
+++ gui/src/gpu/maths.rs
@@ -1,7 +1,5 @@
 use std::f32::consts::PI;
 
-use crate::astro::constants::EARTH_RADIUS;
-
 /// Convert xyz to r, theta, phi for graphical rendering.
 /// x axis is horizontal to the screen
 /// y axis is vertical to the screen
@@ -33,82 +31,34 @@ pub fn wgpu_cartesian_to_spherical(xyz: &[f32; 3]) -> [f32; 3] {
     [r, theta, phi]
 }
 
-// Test these conversions, change theta to pi between -pi & pi
-const MERCATOR_LAT_LIMIT: f32 = std::f32::consts::FRAC_PI_2 - 1e-4;
-
-pub fn lat_lon_to_mercator(altlatlon: &[f32; 3]) -> [f32; 2] {
-    let [_, lat, lon] = altlatlon;
-    let clamped_lat = lat.clamp(-MERCATOR_LAT_LIMIT, MERCATOR_LAT_LIMIT);
-    let x = EARTH_RADIUS * lon;
-    let y = (PI / 4. + clamped_lat / 2.).tan().ln();
-    [x, y]
-}
-
-pub fn mercator_limits() -> [f32; 4] {
-    let y_min = lat_lon_to_mercator(&[0., -MERCATOR_LAT_LIMIT, 0.])[1];
-    let y_max = lat_lon_to_mercator(&[0., MERCATOR_LAT_LIMIT, 0.])[1];
-
-    [0., EARTH_RADIUS * 2. * PI, y_min, y_max]
-}
-
-pub fn mercator_to_uv(mercator: &[f32; 2], mercator_limits: &[f32; 4]) -> [f32; 2] {
-    let [x_min, x_max, y_min, y_max] = mercator_limits;
-    let [x, y] = mercator;
-
-    let u = (x - x_min) / (x_max - x_min);
-    let v = (y_max - y) / (y_max - y_min);
-    [u, v]
-}
-
 #[cfg(test)]
 mod maths_tests {
     use super::*;
 
     #[test]
-    fn test_lat_lon_to_mercator_limits() {
-        let limits = mercator_limits();
-        assert!(limits[2].is_finite());
-        assert!(limits[3].is_finite());
-        assert!(limits[2] < limits[3]);
+    fn test_cartesian_to_spherical_axes() {
+        // +X axis: theta = 0
+        let [r, theta, phi] = wgpu_cartesian_to_spherical(&[1.0, 0.0, 0.0]);
+        assert_eq!(r, 1.0);
+        assert_eq!(theta, 0.0);
+        assert_eq!(phi, std::f32::consts::FRAC_PI_2);
 
-        let top = lat_lon_to_mercator(&[0., std::f32::consts::FRAC_PI_2 - 1e-4, 0.]);
-        let bottom = lat_lon_to_mercator(&[0., -std::f32::consts::FRAC_PI_2 + 1e-4, 0.]);
+        // +Z axis: colatitude 0
+        let [r, theta, phi] = wgpu_cartesian_to_spherical(&[0.0, 0.0, 1.0]);
+        assert_eq!(r, 1.0);
+        assert_eq!(theta, 0.0);
+        assert_eq!(phi, 0.0);
 
-        assert!(top[1].is_finite());
-        assert!(bottom[1].is_finite());
-        assert!(top[1] > bottom[1]);
-    }
+        // +Y axis: theta = pi/2
+        let [r, theta, phi] = wgpu_cartesian_to_spherical(&[0.0, 1.0, 0.0]);
+        assert_eq!(r, 1.0);
+        assert_eq!(theta, std::f32::consts::FRAC_PI_2);
+        assert_eq!(phi, std::f32::consts::FRAC_PI_2);
 
-    #[test]
-    fn test_spherical_to_latlon_mapping() {
-        // top of the sphere should map to v near 0
-        let xyz_top = [0.0, 0.0, 1.0];
-        let rthetaphi = wgpu_cartesian_to_spherical(&xyz_top);
-        let theta = rthetaphi[1];
-        let phi = rthetaphi[2];
-        let latitude = std::f32::consts::FRAC_PI_2 - phi;
-
-        let merc = lat_lon_to_mercator(&[0., latitude, theta]);
-        let uv = mercator_to_uv(&merc, &mercator_limits());
-        assert!(uv[1] >= 0.0 && uv[1] <= 1.0);
-    }
-
-    #[test]
-    fn test_atan2() {
-        /* Setup */
-        let x: f32 = 1.0;
-        let z: f32 = -1.0;
-        let a = x.atan2(z);
-        println!("a: {}", a.to_degrees());
-
-        let a = z.atan2(x);
-        println!("a: {}", a.to_degrees());
-
-        let z: f32 = 1.0;
-        let x: f32 = -1.0;
-        let a = x.atan2(z);
-        println!("a: {}", a.to_degrees());
-        let a = x.atan2(z) % (2. * std::f32::consts::PI);
-        println!("a: {}", a.to_degrees());
+        // -X axis: theta wrapped to pi
+        let [r, theta, phi] = wgpu_cartesian_to_spherical(&[-1.0, 0.0, 0.0]);
+        assert_eq!(r, 1.0);
+        assert_eq!(theta, PI);
+        assert_eq!(phi, std::f32::consts::FRAC_PI_2);
     }
 }
